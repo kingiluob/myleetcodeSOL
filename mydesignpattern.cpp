b0b125c98814056ab1580c69a8753155aa8cdb02
kingil@outlook.com <kingil@foxmail.com>
2019-03-27 21:58:03 +0800

    0327

--- mydesignpattern.cpp
+++ mydesignpattern.cpp
@@ -14,69 +14,70 @@ using namespace std;
 //工厂类 SimpleFactory
 //抽象产品 SimpleProduct
 //具体产品 SimpleProductA、SimpleProductB
+
 //class SimpleProduct
 class SimpleProduct
 {
 public:
-	SimpleProduct();
-	virtual ~SimpleProduct();
-	virtual void use();
+    SimpleProduct();
+    virtual ~SimpleProduct();
+    virtual void use();
 };
 SimpleProduct::SimpleProduct() {}
 SimpleProduct::~SimpleProduct() {}
 void SimpleProduct::use() {}
+
 //class simpleproductA
 class simpleproductA :public SimpleProduct
 {
 public:
-	simpleproductA();
-	virtual ~simpleproductA();
-	virtual void use();
+    simpleproductA();
+    virtual ~simpleproductA();
+    virtual void use();
 };
 simpleproductA::simpleproductA(){}
 simpleproductA::~simpleproductA() {}
 void simpleproductA::use()
 {
-	cout << "use AAA" << endl;
+    cout << "use AAA" << endl;
 }
 class simpleproductB :public SimpleProduct
 {
 public:
-	simpleproductB();
-	virtual ~simpleproductB();
-	virtual void use();
+    simpleproductB();
+    virtual ~simpleproductB();
+    virtual void use();
 };
 simpleproductB::simpleproductB() {}
 simpleproductB::~simpleproductB() {}
 void simpleproductB::use()
 {
-	cout << "use BBB" << endl;
+    cout << "use BBB" << endl;
 }
+
 //class SimpleFactory
 class SimpleFactory
 {
 public:
-	SimpleFactory();
-	virtual ~ SimpleFactory();
-	static SimpleProduct * createProduct(string type);
+    SimpleFactory();
+    virtual ~ SimpleFactory();
+    static SimpleProduct * createProduct(string type);
 };
 SimpleFactory::SimpleFactory() {}
 SimpleFactory::~SimpleFactory() {}
 SimpleProduct * SimpleFactory::createProduct(string type)
 {
-	if (type == "A")
-	{
-		return new simpleproductA();
-	}
-	else if (type == "B")
-	{
-		return new simpleproductB();
-	}
-	else 
-		return NULL;
+    if (type == "A")
+    {
+        return new simpleproductA();
+    }
+    else if (type == "B")
+    {
+        return new simpleproductB();
+    }
+    else
+        return NULL;
 }
-
-
 
 ///单例--------------------------------
 class singleton
@@ -104,132 +105,139 @@ public:
 class Factory;
 class ProductA;
 class ProductB;
-//class ProductA
+
+//lass ProductA
 class ProductA
 {
 public:
-	ProductA();
-	virtual ~ProductA();
-	virtual void use();
+    ProductA();
+    virtual ~ProductA();
+    virtual void use();
 };
 ProductA::ProductA() {};
 ProductA::~ProductA() {};
 void ProductA::use() {};
-//class conProduct
+
+//class conProductA
 class ProductA1:public ProductA
 {
 public:
-	ProductA1();
-	virtual ~ProductA1();
-	virtual void use();
+    ProductA1();
+    virtual ~ProductA1();
+    virtual void use();
 };
 ProductA1::ProductA1() {};
 ProductA1::~ProductA1() {};
 void ProductA1::use()
 {
-	cout << "use AAAA11111" << endl;
+    cout << "use AAAA11111" << endl;
 }
 class ProductA2:public ProductA
 {
 public:
-	ProductA2();
-	virtual ~ProductA2();
-	virtual void use();
+    ProductA2();
+    virtual ~ProductA2();
+    virtual void use();
 };
 ProductA2::ProductA2() {};
 ProductA2::~ProductA2() {};
 void ProductA2::use()
 {
-	cout << "use AAAA22222" << endl;
+    cout << "use AAAA22222" << endl;
 }
+
 //class ProductB
 class ProductB
 {
 public:
-	ProductB();
-	virtual ~ProductB();
-	virtual void use();
+    ProductB();
+    virtual ~ProductB();
+    virtual void use();
 };
 ProductB::ProductB() {};
 ProductB::~ProductB() {};
 void ProductB::use() {};
-//class conProduct
+
+//class conProductB
 class ProductB1 :public ProductB
 {
 public:
-	ProductB1();
-	virtual ~ProductB1();
-	virtual void use();
+    ProductB1();
+    virtual ~ProductB1();
+    virtual void use();
 };
 ProductB1::ProductB1() {};
 ProductB1::~ProductB1() {};
 void ProductB1::use()
 {
-	cout << "useBBBBB11111" << endl;
+    cout << "useBBBBB11111" << endl;
 }
 class ProductB2 :public ProductB
 {
 public:
-	ProductB2();
-	virtual ~ProductB2();
-	virtual void use();
+    ProductB2();
+    virtual ~ProductB2();
+    virtual void use();
 };
 ProductB2::ProductB2() {};
 ProductB2::~ProductB2() {};
 void ProductB2::use()
 {
-	cout << "use BBBB22222" << endl;
+    cout << "use BBBB22222" << endl;
 }
+
 //class Facory
 class Factory
 {
 public:
-	Factory();
-	virtual ~Factory();
-	virtual ProductA * createA()=0;
-	virtual ProductB * createB()=0;
+    Factory();
+    virtual ~Factory();
+    virtual ProductA * createA()=0;
+    virtual ProductB * createB()=0;
 };
 Factory::Factory() {}
 Factory::~Factory() {}
+
 //class ConFactory1
 class ConFactory1 :public Factory
 {
 public:
-	ConFactory1();
-	virtual ~ConFactory1();
-	virtual ProductA * createA();
-	virtual ProductB * createB();
-
+    ConFactory1();
+    virtual ~ConFactory1();
+    virtual ProductA * createA();
+    virtual ProductB * createB();
+    
 };
 ConFactory1::ConFactory1() {};
 ConFactory1::~ConFactory1() {};
 ProductA * ConFactory1::createA()
 {
-	return new ProductA1();
+    return new ProductA1();
 }
 ProductB * ConFactory1::createB()
 {
-	return new ProductB1();
+    return new ProductB1();
 }
+
 //class ConFactory2
 class ConFactory2 :public Factory
 {
 public:
-	ConFactory2();
-	virtual ~ConFactory2();
-	virtual ProductA * createA();
-	virtual ProductB * createB();
-
+    ConFactory2();
+    virtual ~ConFactory2();
+    virtual ProductA * createA();
+    virtual ProductB * createB();
+    
 };
 ConFactory2::ConFactory2() {};
 ConFactory2::~ConFactory2() {};
 ProductA * ConFactory2::createA()
 {
-	return new ProductA2();
+    return new ProductA2();
 }
 ProductB * ConFactory2::createB()
 {
-	return new ProductB2();
+    return new ProductB2();
 }
 
 //中介模式-----------------------------------------------
@@ -243,121 +251,127 @@ class med;
 class member
 {
 public:
-	member();
-	virtual ~member();
-	void setmed(med * themed);
-	virtual void sendmsg(int towho, string msg);
-	virtual void receivemsg(string msg);
+    member();
+    virtual ~member();
+    void setmed(med * themed);
+    virtual void sendmsg(int towho, string msg);
+    virtual void receivemsg(string msg);
 protected:
-	med * _themed;
+    med * _themed;
 };
 //实现member
 member::member() {}
 member::~member() {}
 void member::setmed(med * themed)
 {
-	_themed = themed;
+    _themed = themed;
 }
 void member::sendmsg(int towho, string msg)
 {
-
+    
 }
 void member::receivemsg(string msg)
 {
-	cout << "receive msg is ..." << msg << endl;
+    cout << "receive msg is ..." << msg << endl;
 }
-//med
-class med
-{
-public:
-	med();
-	virtual ~med();
-	virtual void operation(int who, std::string msg);
-	virtual void reg(int who, member* themember);
-};
-//实现med
-med::med() {}
-med::~med() {}
-void med::operation(int who, std::string msg) {}
-void med::reg(int who, member* themember) {}
-//conmed
-class conmed :public med
-{
-public:
-	conmed();
-	virtual ~conmed();
-	virtual void operation(int who, std::string str);
-	virtual void reg(int who, member* themember);
-private:
-	map<int, member*> _membermap;
-};
-//实现conmed
-conmed::conmed() {}
-conmed::~conmed() {}
-void conmed::operation(int who, std::string msg)
-{
-	if (_membermap.find(who) == _membermap.end())
-	{
-		cout << "not found this member" << endl;
-		return;
-	}
-	member * resultwho = _membermap.find(who)->second;
-	resultwho->receivemsg(msg);
-}
-void conmed::reg(int who, member* themember)
-{
-	if (_membermap.find(who) == _membermap.end())
-	{
-		_membermap.insert(make_pair(who, themember));
-		themember->setmed(this);
-	}
-}
+
 //conmember
 class conmemberA :public member
 {
 public:
-	conmemberA();
-	virtual ~conmemberA();
-	virtual void sendmsg(int towho, string msg);
-	virtual void receivemsg(string msg);
+    conmemberA();
+    virtual ~conmemberA();
+    virtual void sendmsg(int towho, string msg);
+    virtual void receivemsg(string msg);
 };
 class conmemberB :public member
 {
 public:
-	conmemberB();
-	virtual ~conmemberB();
-	virtual void sendmsg(int towho, string msg);
-	virtual void receivemsg(string msg);
+    conmemberB();
+    virtual ~conmemberB();
+    virtual void sendmsg(int towho, string msg);
+    virtual void receivemsg(string msg);
 };
 //实现conmemberA
 conmemberA::conmemberA() {}
 conmemberA::~conmemberA() {}
 void conmemberA::sendmsg(int towho, string msg)
 {
-	cout << "sned msg from a to ..." << towho << endl;
-	_themed->operation(towho, msg);
+    cout << "sned msg from a to ..." << towho << endl;
+    _themed->operation(towho, msg);
 }
 void conmemberA::receivemsg(string msg)
 {
-	cout << "A receive msg:.." << msg << endl;
+    cout << "A receive msg:.." << msg << endl;
 }
 conmemberB::conmemberB() {}
 conmemberB::~conmemberB() {}
 void conmemberB::sendmsg(int towho, string msg)
 {
-	cout << "sned msg from a to ..." << towho << endl;
-	_themed->operation(towho, msg);
+    cout << "sned msg from a to ..." << towho << endl;
+    _themed->operation(towho, msg);
 }
 void conmemberB::receivemsg(string msg)
 {
-	cout << "B receive msg:.." << msg << endl;
+    cout << "B receive msg:.." << msg << endl;
 }
+
+//med
+class med
+{
+public:
+    med();
+    virtual ~med();
+    virtual void operation(int who, std::string msg);
+    virtual void reg(int who, member* themember);
+};
+//实现med
+med::med() {}
+med::~med() {}
+void med::operation(int who, std::string msg) {}
+void med::reg(int who, member* themember) {}
+
+//conmed
+class conmed :public med
+{
+public:
+    conmed();
+    virtual ~conmed();
+    virtual void operation(int who, std::string str);
+    virtual void reg(int who, member* themember);
+private:
+    map<int, member*> _membermap;
+};
+//实现conmed
+conmed::conmed() {}
+conmed::~conmed() {}
+void conmed::operation(int who, std::string msg)
+{
+    if (_membermap.find(who) == _membermap.end())
+    {
+        cout << "not found this member" << endl;
+        return;
+    }
+    member * resultwho = _membermap.find(who)->second;
+    resultwho->receivemsg(msg);
+}
+void conmed::reg(int who, member* themember)
+{
+    if (_membermap.find(who) == _membermap.end())
+    {
+        _membermap.insert(make_pair(who, themember));
+        themember->setmed(this);
+    }
+}
+
 
 
 ///代理模式
 ///subject
 ///realsubject
 ///proxy 代理角色
+
+//subject
 class subject
 {
 public:
@@ -368,6 +382,8 @@ public:
 subject::subject(){}
 subject::~subject(){}
 void subject::request(){}
+
+//realsubject
 class realsubject:public subject
 {
 public:
@@ -382,7 +398,7 @@ void realsubject::realrequest()
     std::cout<<"this is real request function..."<<std::endl;
 }
 
-///
+///proxy
 class proxy:subject
 {
 public:
@@ -417,47 +433,45 @@ void proxy::request()
     afterrequest();
 }
 
-
-
 ///观察者模式
 
 int main()
 {
-	//中介模式
-	//cout << "-----" << endl;
-	//conmemberA * pa = new conmemberA();
-	//conmemberB * pb = new conmemberB();
-	//conmed * manager = new conmed();
-	//manager->reg(1,pa);
-	//manager->reg(2,pb);
-	//pa->sendmsg(2,"i am a...");
-	//pb->sendmsg(1,"i am b...");
-	//delete pa, pb, manager;
-
-	//抽象工厂模式
-/*	cout << "-----" << endl;
-	Factory * f1 = new ConFactory1();
-	ProductA * pa = f1->createA();
-	ProductB * pb = f1->createB();
-	pa->use();
-	pb->use();
-	Factory * f2 = new ConFactory2();
-	ProductA * qa = f2->createA();
-	ProductB * qb = f2->createB();
-	qa->use();
-	qb->use();
-	delete f1, f2, pa, pb, qa, qb;*/
-
-	//简单工厂模式
-	//cout << "-----" << endl;
-	//SimpleProduct * prod = SimpleFactory::createProduct("A");//申明为静态，只有一个工厂生产不同产品
-	//prod->use();
-	//delete prod;
-	
-	//代理模式
-	//proxy myproxy;
+    //中介模式
+    //cout << "-----" << endl;
+    //conmemberA * pa = new conmemberA();
+    //conmemberB * pb = new conmemberB();
+    //conmed * manager = new conmed();
+    //manager->reg(1,pa);
+    //manager->reg(2,pb);
+    //pa->sendmsg(2,"i am a...");
+    //pb->sendmsg(1,"i am b...");
+    //delete pa, pb, manager;
+    
+    //抽象工厂模式
+    /*	cout << "-----" << endl;
+     Factory * f1 = new ConFactory1();
+     ProductA * pa = f1->createA();
+     ProductB * pb = f1->createB();
+     pa->use();
+     pb->use();
+     Factory * f2 = new ConFactory2();
+     ProductA * qa = f2->createA();
+     ProductB * qb = f2->createB();
+     qa->use();
+     qb->use();
+     delete f1, f2, pa, pb, qa, qb;*/
+    
+    //简单工厂模式
+    //cout << "-----" << endl;
+    //SimpleProduct * prod = SimpleFactory::createProduct("A");//申明为静态，只有一个工厂生产不同产品
+    //prod->use();
+    //delete prod;
+    
+    //代理模式
+    //proxy myproxy;
     //myproxy.request();
-
-	getchar();
-	return 0;
+    
+    getchar();
+    return 0;
 }
